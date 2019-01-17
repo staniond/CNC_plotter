5e3a3362473f2f34fcb53103734af929089be313
standa <ondra.stanicek@gmail.com>
2019-01-17 15:26:30 +0100

    debug logging only through serial

--- CNC_plotter.h
+++ CNC_plotter.h
@@ -5,21 +5,11 @@
 #define DEBUG
 
 #ifdef DEBUG
-  #ifdef WIFI
     #define Logln(...) Serial.println(__VA_ARGS__)
     #define Log(...) Serial.print(__VA_ARGS__)
-  #else
-    #define Logln(...) Serial.println(__VA_ARGS__)
-    #define Log(...) Serial.print(__VA_ARGS__)
-  #endif
 #else
-  #ifdef WIFI
     #define Logln(...)
     #define Log(...)
-  #else
-    #define Logln(...)
-    #define Log(...)
-  #endif
 #endif
 
 #ifdef WIFI
