5fed9624e7ddd36e0569dbf71fe0b69cbd8b9804
Cpt-Hook <ondra.stanicek@gmail.com>
2019-02-21 14:12:53 +0100

    better bash variable access?

--- misc/virtual_serial.sh
+++ misc/virtual_serial.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 ADDRESS=${1:-192.168.0.36:8888}
 FILE=${2:-./ttyUSB0}
-echo Connecting $ADDRESS with $FILE
+echo Connecting ${ADDRESS} with ${FILE}
 while :
 do
-	socat pty,link=$FILE,waitslave tcp:$ADDRESS
+	socat pty,link=${FILE},waitslave tcp:${ADDRESS}
 done
-
