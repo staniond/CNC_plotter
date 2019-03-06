68dfb87f885104b50aca83ce25425f8f8506f5ce
standa <ondra.stanicek@gmail.com>
2019-03-06 15:21:54 +0100

    always print ip and port to serial

--- main/wifi.c
+++ main/wifi.c
@@ -58,6 +58,7 @@ static esp_err_t event_handler(void *ctx, system_event_t *event) {
 
             char *ip4 = ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip);
             ESP_LOGI(TAG, "IPv4: %s", ip4);
+            printf("Connected with ip: %s (port: %d)\n", ip4, TCP_PORT);
             break;
         case SYSTEM_EVENT_STA_DISCONNECTED:
             /* This is a workaround as ESP32 WiFi libs don't currently auto-reassociate. */
